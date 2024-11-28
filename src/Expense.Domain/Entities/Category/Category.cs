238de066181fd5017ad798a3ec38fec2bbcda6d5
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-28 22:17:45 +0500

    Category Entity

---
+++ src/Expense.Domain/Entities/Category/Category.cs
@@ -0,0 +1,8 @@
+﻿using Expense.Domain.Entities.Abstractions;
+
+namespace Expense.Domain.Entities.Category;
+
+public sealed class Category : Entity<ulong>
+{
+    public string Name { get; set; } = default!;
+}
