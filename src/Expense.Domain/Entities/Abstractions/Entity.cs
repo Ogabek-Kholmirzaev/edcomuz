9f9b1d51620ee0f257f19e6596df23e62840bcac
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-28 22:14:07 +0500

    Abstract Entity

---
+++ src/Expense.Domain/Entities/Abstractions/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace Expense.Domain.Entities.Abstractions;
+
+public abstract class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
