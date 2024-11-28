2e6fe7e2215e440aa1661b75158aa8a648d50a04
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-28 23:01:37 +0500

    Changes in Entity and Outlay

--- src/Expense.Domain/Entities/Abstractions/Entity.cs
+++ src/Expense.Domain/Entities/Abstractions/Entity.cs
@@ -3,6 +3,6 @@
 public abstract class Entity<TId>
 {
     public TId Id { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
 }
--- src/Expense.Domain/Entities/Outlays/Outlay.cs
+++ src/Expense.Domain/Entities/Outlays/Outlay.cs
@@ -6,6 +6,7 @@ namespace Expense.Domain.Entities.Outlays;
 public sealed class Outlay : Entity<ulong>
 {
     public ulong CategoryId { get; set; }
+    public DateOnly Date { get; set; }
     public decimal Price { get; set; }
     public string? Comment { get; set; }
 
