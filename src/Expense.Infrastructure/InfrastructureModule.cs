cc35b385577d884e0c554d6bd442209886251bbc
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-29 14:55:25 +0500

    InfrastructureModule

---
+++ src/Expense.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,19 @@
+﻿using Expense.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Expense.Infrastructure;
+
+public static class InfrastructureModule
+{
+    public static IServiceCollection AddInfrastructureModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database")
+            ?? throw new ArgumentNullException("Database connection string is null");
+
+        services.AddDbContext<OutlayDbContext>(options => options.UseNpgsql(connectionString));
+
+        return services;
+    }
+}
