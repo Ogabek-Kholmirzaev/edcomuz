b7ef9244d2783c6da305b87cd66cc21638c409e2
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-30 02:16:19 +0500

    Categories Seed Data Migration

--- src/Expense.Infrastructure/Data/Migrations/20241129211218_Caterogiries_Table_Seed_Data.Designer.cs
+++ src/Expense.Infrastructure/Data/Migrations/20241129211218_Caterogiries_Table_Seed_Data.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Expense.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Expense.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(OutlayDbContext))]
-    partial class OutlayDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241129211218_Caterogiries_Table_Seed_Data")]
+    partial class Caterogiries_Table_Seed_Data
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Expense.Infrastructure/Data/Migrations/20241129211218_Caterogiries_Table_Seed_Data.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Expense.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Caterogiries_Table_Seed_Data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "CreatedAt", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1m, new DateTime(2024, 11, 29, 21, 12, 18, 433, DateTimeKind.Utc).AddTicks(2404), "Oziq-ovqat", null },
+                    { 2m, new DateTime(2024, 11, 29, 21, 12, 18, 433, DateTimeKind.Utc).AddTicks(2407), "Transport", null },
+                    { 3m, new DateTime(2024, 11, 29, 21, 12, 18, 433, DateTimeKind.Utc).AddTicks(2408), "Mobil aloqa", null },
+                    { 4m, new DateTime(2024, 11, 29, 21, 12, 18, 433, DateTimeKind.Utc).AddTicks(2410), "Internet", null },
+                    { 5m, new DateTime(2024, 11, 29, 21, 12, 18, 433, DateTimeKind.Utc).AddTicks(2411), "O'yin-kulgi", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1m);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2m);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 3m);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 4m);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 5m);
+        }
+    }
+}
