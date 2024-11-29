4f4e79a569db88f7529f0dfc28b8acc83e4bb5a6
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-29 20:51:37 +0500

    CQRS Abstractions

---
+++ src/Expense.Application/Abstractions/CQRS/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Expense.Application.Abstractions.CQRS;
+
+public interface ICommand : ICommand<Unit>
+{
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
