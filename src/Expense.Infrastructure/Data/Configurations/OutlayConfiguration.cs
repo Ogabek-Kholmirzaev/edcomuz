7b258197296d380a82bae837c6400b0a0858fb15
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-30 20:31:42 +0500

    Outlays Seed Data

--- src/Expense.Infrastructure/Data/Configurations/OutlayConfiguration.cs
+++ src/Expense.Infrastructure/Data/Configurations/OutlayConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Expense.Domain.Entities.Outlays;
+﻿using Bogus;
+using Expense.Domain.Entities.Outlays;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -16,7 +17,7 @@ public class OutlayConfiguration : IEntityTypeConfiguration<Outlay>
         builder.Property(outlay => outlay.Date)
             .IsRequired()
             .HasConversion(
-                dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue),
+                dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue, DateTimeKind.Utc),
                 dateTime => DateOnly.FromDateTime(dateTime));
 
         builder.Property(outlay => outlay.Price)
@@ -28,5 +29,32 @@ public class OutlayConfiguration : IEntityTypeConfiguration<Outlay>
 
         builder.Property(outlay => outlay.Comment)
             .HasMaxLength(200);
+
+        builder.HasData(GetOutlays());
+    }
+
+    private IEnumerable<Outlay> GetOutlays()
+    {
+        var outlays = new List<Outlay>();
+        var faker = new Faker();
+
+        for (long i = 1; i <= 1000; i++)
+        {
+            var date = faker.Date.BetweenDateOnly(new DateOnly(2024, 1, 1), new DateOnly(2024, 12, 31));
+            
+            outlays.Add(new()
+            {
+                Id = i,
+                CategoryId = faker.Random.Long(1, 5),
+                Date = date,
+                Price = faker.Random.Decimal(1, 200),
+                Comment = faker.Commerce.ProductDescription(),
+                CreatedAt = faker.Date.Between(
+                    date.ToDateTime(TimeOnly.MinValue, DateTimeKind.Utc),
+                    date.AddDays(2).ToDateTime(TimeOnly.MinValue, DateTimeKind.Utc))
+            });
+        }
+        
+        return outlays;
     }
 }
