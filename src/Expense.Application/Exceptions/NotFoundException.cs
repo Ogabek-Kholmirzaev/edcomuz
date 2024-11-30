2164fe45f04f359caa2b0b30f2e50de13b7050b8
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-11-30 06:08:39 +0500

    NotFoundException

---
+++ src/Expense.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+namespace Expense.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message)
+    {
+    }
+
+    public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) not found")
+    {
+    }
+}
