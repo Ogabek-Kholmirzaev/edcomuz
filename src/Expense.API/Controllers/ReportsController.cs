28364e74ae8cb157624f710893c38b2b5ab9fb3d
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-02 04:56:08 +0500

    GetReportByYear Endpoint

---
+++ src/Expense.API/Controllers/ReportsController.cs
@@ -0,0 +1,19 @@
+﻿using Expense.Application.Reports.GetReportsByYear;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expense.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReportsController(ISender sender) : ControllerBase
+{
+    [HttpGet("year/{year:int}")]
+    public async Task<ActionResult<GetReportsByYearResult>> GetByYear(int year)
+    {
+        var query = new GetReportsByYearQuery(year);
+        var respone = await sender.Send(query);
+
+        return Ok(respone);
+    }
+}
