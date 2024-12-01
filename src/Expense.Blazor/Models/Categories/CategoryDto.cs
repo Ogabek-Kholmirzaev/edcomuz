f67b227454f412e57a5d75fb90296f4f31095acf
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-01 17:57:12 +0500

    UpdateCategory Page

--- src/Expense.Blazor/Models/Categories/CategoryDto.cs
+++ src/Expense.Blazor/Models/Categories/CategoryDto.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Expense.Blazor.Models.Categories;
 
 public class CategoryDto
 {
     public long Id { get; set; }
+
+    [Required(ErrorMessage = "Category name is required.")]
+    [StringLength(100, ErrorMessage = "Name cannot exeed 100 characters.")]
     public string Name { get; set; } = default!;
+
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }
