2f5fb44c85665bacb0d6e6cf3d770f292996bfc3
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-01 18:13:36 +0500

    Toastr Notification

--- src/Expense.Blazor/Program.cs
+++ src/Expense.Blazor/Program.cs
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7165/") });
 builder.Services.AddScoped<CategoryService>();
 builder.Services.AddScoped<NavigationService>();
+builder.Services.AddScoped<ToastrService>();
 
 await builder.Build().RunAsync();
---
+++ src/Expense.Blazor/Services/ToastrService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.JSInterop;
+
+namespace Expense.Blazor.Services;
+
+public class ToastrService
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public ToastrService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task ShowSuccess(string message, string title = "Success")
+    {
+        await _jsRuntime.InvokeVoidAsync("toastr.success", message, title);
+    }
+
+    public async Task ShowError(string message, string title = "Error")
+    {
+        await _jsRuntime.InvokeVoidAsync("toastr.error", message, title);
+    }
+
+    public async Task ShowWarning(string message, string title = "Warning")
+    {
+        await _jsRuntime.InvokeVoidAsync("toastr.warning", message, title);
+    }
+
+    public async Task ShowInfo(string message, string title = "Info")
+    {
+        await _jsRuntime.InvokeVoidAsync("toastr.info", message, title);
+    }
+}
