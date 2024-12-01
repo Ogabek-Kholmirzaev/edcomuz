101c41d7c8c19093e3c8e52c75045e5f244a5dcc
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-02 04:27:14 +0500

    UI Chart Sample

---
+++ src/Expense.Blazor/wwwroot/js/Chart.js
@@ -0,0 +1,15 @@
+﻿function GeneratePieChart(expenseInformations) {
+    am4core.useTheme(am4themes_animated);
+
+    var chart = am4core.create("chartdiv", am4charts.PieChart3D);
+
+    chart.hiddenState.properties.opacity = 0;
+    chart.legend = new am4charts.Legend();
+
+    chart.data = expenseInformations;
+
+    var series = chart.series.push(new am4charts.PieSeries3D());
+
+    series.dataFields.value = "totalPrice";
+    series.dataFields.category = "name";
+}
