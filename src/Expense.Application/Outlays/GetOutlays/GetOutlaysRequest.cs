7779aa37558fc5891df867b477625275bfcb70eb
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-02 07:23:19 +0500

    GetOutlaysRequest

---
+++ src/Expense.Application/Outlays/GetOutlays/GetOutlaysRequest.cs
@@ -0,0 +1,11 @@
+﻿using Expense.Application.Pagination;
+
+namespace Expense.Application.Outlays.GetOutlays;
+
+public record GetOutlaysRequest(
+    long? CategoryId,
+    string? Comment,
+    DateOnly DateFrom,
+    DateOnly DateTo,
+    decimal? PriceFrom,
+    decimal? PriceTo) : PaginationRequest;
