f1557d23068c7ca6cd17dc2a54ce058f590d103d
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-12-02 05:03:01 +0500

    UI ReportDto

---
+++ src/Expense.Blazor/Models/Reports/ReportDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Expense.Blazor.Models.Reports;
+
+public class ReportDto
+{
+    public int Year { get; set; }
+    public string MonthName { get; set; } = default!;
+    public string TotalPrice { get; set; } = default!;
+    public string TotalCount { get; set; } = default!;
+}
